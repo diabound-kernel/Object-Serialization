b9d795a8908400e9ce22f0fdced54344c4eb64be
diabound-kernel <dammisa01@gmail.com>
2022-12-24 08:14:48 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#define FMT_HEADER_ONLY
+#include "fmt/ostream.h"
+
+#include "introspection/json.hpp"
+#include "introspection/yaml.hpp"
+
+#include <string>
+
+struct Socket
+{
+    std::string address;
+    int port;
+    std::string state;
+
+    GEN_METADATA(address, port, state)
+};
+
+int main()
+{
+    auto socket = YAML::LoadFile("../socket.yaml").as<Socket>();
+
+    fmt::print("{}\n", nlohmann::json(socket).dump());
+
+    return 0;
+}
